24a7280f8b783ef89b2443a3840496db0a058b82
Hamish Coleman <hamish@zot.org>
2018-04-16 13:00:35 +1000

    Rescue file from archives

--- c_fast.c
+++ c_fast.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+char outbuf[4096];
+
 const char fizz[] = "fizz";
 const char buzz[] = "buzz";
 const char nl[] = "\n";
@@ -25,24 +27,38 @@ int main(int argc, char **argv) {
     int trigger2 = 5;
     int i = 0;
 
+    char *p = outbuf;
+    int plen = 0;
+
     while(i <= 101) {
         int flag = 0;
         if (i == trigger1) {
-            write(1,fizz,sizeof(fizz)-1);
+            memcpy(p, fizz, sizeof(fizz)-1);
+            p += sizeof(fizz)-1;
+            plen += sizeof(fizz)-1;
+
             trigger1 += 3;
             flag = 1;
         }
         if (i == trigger2) {
-            write(1,buzz,sizeof(buzz)-1);
+            memcpy(p, buzz, sizeof(buzz)-1);
+            p += sizeof(buzz)-1;
+            plen += sizeof(buzz)-1;
+
             trigger2 += 5;
             flag = 1;
         }
         if (!flag) {
             int digits;
-            char *p = itoa(i,&digits,10);
-            write(1,p,digits);
+            char *a = itoa(i,&digits,10);
+
+            memcpy(p, a, digits);
+            p += digits;
+            plen += digits;
         }
-        write(1,nl,sizeof(nl)-1);
+        *p++ = '\n';
+        plen++;
         i++;
     }
+    write(1, outbuf, plen);
 }
