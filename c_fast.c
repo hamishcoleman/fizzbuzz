869e427c57ec13be0d1d362c9d31b9237b0afb14
Hamish Coleman <hamish@zot.org>
2018-04-16 12:52:09 +1000

    Rescue file from archives

--- c_fast.c
+++ c_fast.c
@@ -7,15 +7,17 @@ const char fizz[] = "fizz";
 const char buzz[] = "buzz";
 const char nl[] = "\n";
 
-int itoa(int i, char *buf, int radix) {
-    int digits = 0;
+char _itoabuf[10];
+char *itoa(int i, int *digits, int radix) {
+    char *p = _itoabuf + sizeof(_itoabuf);
+    *p = 0;
+    *digits = 0;
     do {
-        *buf++ = i%radix + '0';
+        *--p = i%radix + '0'; // FIXME radix>10
         i /= radix;
-        digits ++;
+        (*digits) ++;
     } while(i);
-    *buf = 0;
-    return digits;
+    return p;
 }
 
 int main(int argc, char **argv) {
@@ -23,7 +25,7 @@ int main(int argc, char **argv) {
     int trigger2 = 5;
     int i = 0;
 
-    while(i <= 100) {
+    while(i <= 101) {
         int flag = 0;
         if (i == trigger1) {
             write(1,fizz,sizeof(fizz)-1);
@@ -36,9 +38,9 @@ int main(int argc, char **argv) {
             flag = 1;
         }
         if (!flag) {
-            char buf[10];
-            int digits = itoa(i,buf,10);
-            write(1,buf,digits);
+            int digits;
+            char *p = itoa(i,&digits,10);
+            write(1,p,digits);
         }
         write(1,nl,sizeof(nl)-1);
         i++;
