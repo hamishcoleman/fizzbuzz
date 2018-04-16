aaaf30d1219d1dd38aed1201f9aafabafd345a21
Hamish Coleman <hamish@zot.org>
2018-04-16 17:59:22 +1000

    Rescue file from archives

--- c_fast.c
+++ c_fast.c
@@ -1,23 +1,25 @@
 
 #include <unistd.h>
 
-char outbuf[4096];
+#define BUFLEN 4096
+struct buffer {
+    int pos;
+    char data[BUFLEN];
+};
 
 const char fizz[] = "fizz";
 const char buzz[] = "buzz";
 
-void flush(char *buf, int *buflen, char **bufptr) {
-    write(1, buf, *buflen);
-    *buflen = 0;
-    *bufptr = buf;
+void flush(struct buffer *buf) {
+    write(1, buf->data, buf->pos);
+    buf->pos = 0;
 }
 
-char *append(const char *buf, int *buflen, char *bufptr) {
-    while (*buf) {
-        *bufptr++ = *buf++;
-        (*buflen)++;
+void append(struct buffer *buf, const char *data) {
+    while (*data) {
+        // FIXME - should cmp pos with sizeof(data) and flush() if needed
+        buf->data[ buf->pos++ ] = *data++;
     }
-    return bufptr;
 }
 
 char _itoabuf[10];
@@ -32,35 +34,35 @@ char *itoa(int i, int radix) {
 }
 
 int main(int argc, char **argv) {
+    struct buffer outbuf;
+    outbuf.pos = 0;
+
     int trigger1 = 3;
     int trigger2 = 5;
     int i = 0;
-
-    char *p = outbuf;
-    int plen = 0;
 
     while(i <= 101) {
         int flag = 0;
 
         if (i == trigger1) {
-            p = append(fizz, &plen, p);
+            append(&outbuf, fizz);
 
             trigger1 += 3;
             flag = 1;
         }
         if (i == trigger2) {
-            p = append(buzz, &plen, p);
+            append(&outbuf, buzz);
 
             trigger2 += 5;
             flag = 1;
         }
         if (!flag) {
             const char *s = itoa(i, 10);
-            p = append(s, &plen, p);
+            append(&outbuf, s);
         }
 
-        p = append("\n", &plen, p);
+        append(&outbuf, "\n");
         i++;
     }
-    flush(outbuf, &plen, &p);
+    flush(&outbuf);
 }
